6ce7d5aa53f000271bb27279b1665165101717b2
Samuel O'Malley <git@sam.omalley.cc>
2025-03-15 15:50:03 +1030

    Use a staging buffer for efficiency

--- src/main.rs
+++ src/main.rs
@@ -599,14 +599,34 @@ unsafe fn create_vertex_buffer(
     data: &mut AppData,
 ) -> Result<()> {
     let size = (size_of::<Vertex>() * VERTICES.len()) as u64;
+    let (staging_buffer, staging_buffer_memory) = unsafe {
+        create_buffer(
+            instance,
+            device,
+            data,
+            size,
+            vk::BufferUsageFlags::TRANSFER_SRC,
+            vk::MemoryPropertyFlags::HOST_COHERENT | vk::MemoryPropertyFlags::HOST_VISIBLE,
+        )?
+    };
+
+    unsafe {
+        let memory =
+            device.map_memory(staging_buffer_memory, 0, size, vk::MemoryMapFlags::empty())?;
+
+        memcpy(VERTICES.as_ptr(), memory.cast(), VERTICES.len());
+
+        device.unmap_memory(staging_buffer_memory);
+    }
+
     let (vertex_buffer, vertex_buffer_memory) = unsafe {
         create_buffer(
             instance,
             device,
             data,
             size,
-            vk::BufferUsageFlags::VERTEX_BUFFER,
-            vk::MemoryPropertyFlags::HOST_COHERENT | vk::MemoryPropertyFlags::HOST_VISIBLE,
+            vk::BufferUsageFlags::TRANSFER_DST | vk::BufferUsageFlags::VERTEX_BUFFER,
+            vk::MemoryPropertyFlags::DEVICE_LOCAL,
         )?
     };
 
@@ -614,12 +634,51 @@ unsafe fn create_vertex_buffer(
     data.vertex_buffer_memory = vertex_buffer_memory;
 
     unsafe {
-        let memory =
-            device.map_memory(vertex_buffer_memory, 0, size, vk::MemoryMapFlags::empty())?;
+        // Copy data from staging buffer to vertex buffer.
+        copy_buffer(device, data, staging_buffer, vertex_buffer, size)?;
 
-        memcpy(VERTICES.as_ptr(), memory.cast(), VERTICES.len());
+        // Clean up staging buffer.
+        device.destroy_buffer(staging_buffer, None);
+        device.free_memory(staging_buffer_memory, None);
+    }
 
-        device.unmap_memory(vertex_buffer_memory);
+    Ok(())
+}
+
+unsafe fn copy_buffer(
+    device: &Device,
+    data: &AppData,
+    source: vk::Buffer,
+    destination: vk::Buffer,
+    size: vk::DeviceSize,
+) -> Result<()> {
+    let info = vk::CommandBufferAllocateInfo::builder()
+        .level(vk::CommandBufferLevel::PRIMARY)
+        .command_pool(data.command_pool)
+        .command_buffer_count(1);
+
+    let command_buffer = unsafe { device.allocate_command_buffers(&info)?[0] };
+
+    let info =
+        vk::CommandBufferBeginInfo::builder().flags(vk::CommandBufferUsageFlags::ONE_TIME_SUBMIT);
+
+    unsafe {
+        device.begin_command_buffer(command_buffer, &info)?;
+    }
+
+    let regions = vk::BufferCopy::builder().size(size);
+    unsafe {
+        device.cmd_copy_buffer(command_buffer, source, destination, &[regions]);
+        device.end_command_buffer(command_buffer)?;
+    }
+
+    let command_buffers = &[command_buffer];
+    let info = vk::SubmitInfo::builder().command_buffers(command_buffers);
+
+    unsafe {
+        device.queue_submit(data.graphics_queue, &[info], vk::Fence::null())?;
+        device.queue_wait_idle(data.graphics_queue)?;
+        device.free_command_buffers(data.command_pool, &[command_buffer]);
     }
 
     Ok(())
